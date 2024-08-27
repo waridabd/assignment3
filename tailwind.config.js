59bb441cb054f79a4e643b22665943fb3849dbfb
waridabd <waridabd20@gmail.com>
2024-08-27 22:04:36 +0600

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
     theme: {
       extend: {
-        backgroundImage: {
-          'hero-pattern': "url('/assets/bg.jpg')",
-          'footer-texture': "url('/img/footer-texture.png')",
-        }
+       
       }
     }
   }
